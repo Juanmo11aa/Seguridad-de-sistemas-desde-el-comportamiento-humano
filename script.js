e2cd1c3b907d405ec26562b996997e9e270fd414
David-bot-03 <davsbenavidesl124@umariana.edu.co>
2025-08-15 09:30:12 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ const slides = [
    <p>En sus inicios en os sistemas la seguridad se limitaba a contraseñas simples y entornos donde 
    solo algunas personas podian interactuar con esta. algo como las interfaces basicas y poco amigables con el usuario
    las cuales eran pensadas para expertos ademas de algunas interfaces servian solo escribiendo comandos.
-
-
+ 
+   <br> <img src="https://media.licdn.com/dms/image/v2/D5612AQFSucByB9jEyg/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1658517568493?e=2147483647&v=beta&t=4kC9rcy-_wtdmJEtdFAZ9bHiPurX7vTZIzzz3o1DIsQ" alt="Evolución de las interfaces de computadora" style="width:100%; max-width:500px; display:block; margin:auto; margin-top:15px; border-radius:8px;">
+  
    Con la aparicion de las interfaces y el desarrollo tecnologico el usar un computador se volvio muy util y simple 
    lo que se muestra gracias a que su uso se volvio global casi para cualquier persona y con el pasar del tiempo se mejoro la interaccion
    con los usuarios.
@@ -20,26 +21,33 @@ const slides = [
    tareas más rápidas, accesibles y prácticas para el día a día.
    
    Ademas de esto muchas personas pueden llegar a descuidar sus datos y hacen que sus credenciales sean de facil acceso para algunos terceros 
-   con intenciones maliciosas</p>`,
+   con intenciones maliciosas
+      <br> <img src="https://images.squarespace-cdn.com/content/v1/5760269145bf21440773e26c/1632234327098-0EW2DTFZQFXJXW3B4PCF/computer+blog.jpg?format=2500w" alt="Evolución de las interfaces de computadora" style="width:100%; max-width:500px; display:block; margin:auto; margin-top:15px; border-radius:8px;">
+
+   </p>`,
  
   `<h2>Errores y aprendizajes del usuario </h2>
 
 <p> Los fallos de seguridad en los usuarios son resultado de limitaciones mentales como la memoria y la concentración. Por lo tanto, el papel de las personas es clave en la protección de los sistemas.
 Por ejemplo, los usuarios optan por contraseñas poco seguras porque son más fáciles de recordar, o pasan por alto las alertas de seguridad en los sitios web.
-  </p>`,
+  <br> <img src="https://images.squarespace-cdn.com/content/v1/5760269145bf21440773e26c/3d33d092-b2db-4b23-8491-a6a013e8a670/unsplash-image-1K9T5YiZ2WU.jpg?format=2500w" alt="Evolución de las interfaces de computadora" style="width:100%; max-width:500px; display:block; margin:auto; margin-top:15px; border-radius:8px;">
+</p>`,
 
    
 
   `<h2>Retos de diseño centrado en el ser humano</h2>
    <ul>
   El diseño en la actualidad tiene como objetivo desarrollar sistemas que sean flexibles, seguros y sencillos de utilizar, con un énfasis en la experiencia del usuario. Las dificultades más importantes son controlar la carga mental para prevenir la sobrecarga de información, crear diseños que se adapten a diferentes habilidades y garantizar que los usuarios tengan un método sencillo para resguardar su información personal.
-   </ul>`,
+   <br> <img src="https://ovacen.com/wp-content/uploads/2022/12/software-diseno-interfaz-usuarios.jpg" alt="Evolución de las interfaces de computadora" style="width:100%; max-width:500px; display:block; margin:auto; margin-top:15px; border-radius:8px;">
+  </ul>`,
 
   `<h2>Disciplinas que aportan a su rama
  </h2>
    <p> Ramas como lo son:
-   Psicologia,Eragnomia,Diseño,Sociologia,estadistica han ayudaso a el desarrollo d ela tecologia como se la conoce hoy en dia
-   Ademas de estas ramas tambien se puede obtener mejoras y ayuda de ramas como lo son las ingenierias las cuales optimisan el funcionamiento de un sistema</p>`,
+   Psicologia,Ergnomia,Diseño,Sociologia y estadistica han ayudado a el desarrollo de la tecologia como se la conoce hoy en dia
+   Ademas de estas ramas tambien se puede obtener mejoras y ayuda de ramas como lo son las ingenierias las cuales optimisan el funcionamiento de un sistema
+   <br> <img src="https://worldcampus.saintleo.edu/img/article/diferencias-entre-ingeniero-en-sistemas-e-ingeniero-en-computacion.webp" alt="Evolución de las interfaces de computadora" style="width:100%; max-width:500px; display:block; margin:auto; margin-top:15px; border-radius:8px;">
+   </p>`,
 
   `<h2>trabajo multidisciplinario real </h2>
    <p> 
@@ -49,6 +57,7 @@ Gracias a esta colaboración, hoy los cajeros son más fáciles de usar, seguros
 
 Otro ejemplo seria el desarrollo de el computador en la cual tuvo que existir relacion y comunicacion entre ingenieros, psiocologos,expertos en ergonomia entre otros para que hoy en dia 
 toda persona tenga un uso practico y eficiente 
+<br> <img src="https://www.bancocajasocial.com/content/dam/bcs/contenido/imagenes-blog/vRuMHT0yeH9lZlqtlqCv.jpg" alt="Evolución de las interfaces de computadora" style="width:100%; max-width:500px; display:block; margin:auto; margin-top:15px; border-radius:8px;">
 </p>`,
 
   `<h2>Conexion con el libro de Alan Dix </h2>
@@ -57,6 +66,7 @@ Vulnerabilidad de los dispositivos: Los teclados y sensores (como los input devi
 Riesgo humano: La inseguridad no solo viene de los dispositivos, sino también de los usuarios. Un ejemplo es el método de ataque "mirar por encima del hombro" (shoulder surfing), que compromete contraseñas en lugares públicos.
 Importancia del Feedback Claro: Los sistemas de seguridad deben ofrecer una retroalimentación clara y visible al usuario. Una mala retroalimentación hace que se ignoren alertas importantes.
 Ejemplo: El ícono del candado en las páginas web HTTPS es un tipo de feedback visual que confirma una conexión segura y ayuda a prevenir ataques de phishing.
+<br> <img src="https://media.istockphoto.com/id/898222360/es/foto/presione-el-bot%C3%B3n-enter-en-el-equipo-rojo-de-virus-protector-del-teclado-exclamaci%C3%B3n.jpg?s=612x612&w=0&k=20&c=VjU9jRpFObNZxDdeMbucQf_GpRE7LhKU0nYo3cdoA6Y=" alt="Evolución de las interfaces de computadora" style="width:100%; max-width:500px; display:block; margin:auto; margin-top:15px; border-radius:8px;">
 </p>`,
 
 `<h2>Proyeccion a futuro </h2>
@@ -69,6 +79,7 @@ Beneficios y Desafíos
 Seguridad fluida: Deja de ser un obstáculo y se integra en la experiencia del usuario.
 Protección automática: El sistema se adapta al entorno para ajustar el nivel de seguridad.
 Debate pendiente: Esta tecnología plantea importantes discusiones sobre la privacidad y el control de datos.
+<br> <img src="https://c.files.bbci.co.uk/F12A/production/_106083716_1920illustration-1f-def-hr.jpg" alt="Evolución de las interfaces de computadora" style="width:100%; max-width:500px; display:block; margin:auto; margin-top:15px; border-radius:8px;">
 </p>`,
 
 ];
